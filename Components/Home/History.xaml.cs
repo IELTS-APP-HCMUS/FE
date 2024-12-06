0a46f9c630609e4bd9969c6eb67f3b47c4144448
Mai Nhật Nam <22120219@student.hcmus.edu.vn>
2024-12-06 11:08:01 +0700

    Revert the History board to the correct one

--- Components/Home/History.xaml.cs
+++ Components/Home/History.xaml.cs
@@ -15,7 +15,6 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using login_full.Services;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.UI;
 
 
 // To learn more about WinUI, the WinUI project structure,
@@ -23,34 +22,21 @@ using Microsoft.UI;
 
 namespace login_full.Components.Home
 {
-    public sealed partial class History : UserControl
-    {
-        public HistoryViewModel ViewModel { get; }
-        public History()
-        {
-            this.InitializeComponent();
+	public sealed partial class History : UserControl
+	{
+		public HistoryViewModel ViewModel { get; }
+		public History()
+		{
+			this.InitializeComponent();
 
-            ViewModel = ServiceLocator.GetService<HistoryViewModel>();
-            this.DataContext = ViewModel;
-            this.Loaded += OnLoaded;
-        }
-        private void OnLoaded(object sender, RoutedEventArgs e)
-        {
-            ViewModel.RefreshHistory();
-        }
-        private void ByNameTab_Click(object sender, RoutedEventArgs e)
-		{
-			ByTimeTab.Background = new SolidColorBrush(Colors.White);
-			ByTimeTab.Foreground = new SolidColorBrush(Colors.DarkCyan);
-			ByNameTab.Background = new SolidColorBrush(Colors.Beige);
-            ByNameTab.Foreground = new SolidColorBrush(Colors.Orange);
+			ViewModel = ServiceLocator.GetService<HistoryViewModel>();
+			this.DataContext = ViewModel;
+			this.Loaded += OnLoaded;
 		}
-		private void ByTimeTab_Click(object sender, RoutedEventArgs e)
+
+		private void OnLoaded(object sender, RoutedEventArgs e)
 		{
-			ByNameTab.Background = new SolidColorBrush(Colors.White);
-			ByNameTab.Foreground = new SolidColorBrush(Colors.DarkCyan);
-			ByTimeTab.Background = new SolidColorBrush(Colors.Beige);
-			ByTimeTab.Foreground = new SolidColorBrush(Colors.Orange);
+			ViewModel.RefreshHistory();
 		}
 	}
 }
